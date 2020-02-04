1434a62dff7084959095caf4bf3462b7bda9d0ac
Mary Lang <mcl86@cornell.edu>
2020-02-03 19:30:03 -0700

    Add travis yml file

--- app/models/favorite.js
+++ app/models/favorite.js
@@ -2,9 +2,14 @@ const environment = process.env.NODE_ENV || 'development';
 const configuration = require('../../knexfile')[environment];
 const database = require('knex')(configuration);
 
-const all = () => database('favorites')
-  .select()
+class Favorite {
 
-module.exports = {
-  all,
+  constructor(result) {
+    this.title = result.track.track_name
+    this.artistName = result.track.artist_name
+    this.genre = result.track.primary_genres.music_genre_list[0].music_genre.music_genre_name || 'Unknown'
+    this.rating = result.track.track_rating
+  }
 }
+
+module.exports = Favorite;
