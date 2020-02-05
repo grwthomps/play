f450a73e8bf5960993946f803cb3fce58b8e6593
Mary Lang <mcl86@cornell.edu>
2020-02-04 18:55:42 -0700

    Add seeds

--- db/seeds/favorites.js
+++ db/seeds/favorites.js
@@ -3,6 +3,9 @@ exports.seed = function(knex) {
     .then(() => {
       return Promise.all([
         knex('favorites').insert([
+          {title: 'Alive', artistName: 'Dabin', rating: 75, genre: 'edm'},
+          {title: 'Time', artistName: 'Kidswaste', rating: 90, genre: 'alternative'},
+          {title: 'Breathe', artistName: 'Telepopmusik', rating: 80, genre: 'down tempo'}
           {title: 'Shake It Off', artistName: 'Taylor Swift', genre: 'Pop', rating: 95},
           {title: 'Get Out', artistName: 'Chvrches', genre: 'Pop', rating: 92},
           {title: "Sing About Me / I'm Dying of Thirst", artistName: 'Kendrick Lamar', genre: 'Rap', rating: 85},
