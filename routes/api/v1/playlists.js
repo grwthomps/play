99fba2a4fa8711214c4483be5c77244c23d0cb33
Graham Thompson <grwthomps@gmail.com>
2020-02-06 16:02:38 -0700

    Create post route for playlist
    Co-Authored-By: Mary Lang <mcat56@users.noreply.github.com>

--- routes/api/v1/playlists.js
+++ routes/api/v1/playlists.js
@@ -1,8 +1,30 @@
 const environment = process.env.NODE_ENV || 'development';
 const configuration = require('../../../knexfile')[environment];
 const database = require('knex')(configuration);
-require('dotenv').config('/.env')
 
 
 const express = require('express');
 const router = express.Router();
+
+router.post('/', function(req, res) {
+  if(req.body.title) {
+    var info = {title: req.body.title}
+    database('playlists').insert(info, ['id', 'title', 'created_at', 'updated_at'])
+      .then((playlist) =>{
+        var formatted_playlist = {
+          id: playlist[0].id,
+          title: playlist[0].title,
+          createdAt: playlist[0].created_at,
+          updatedAt: playlist[0].updated_at
+        }
+        return res.status(201).json(formatted_playlist)
+      })
+      .catch((error) => {
+        return res.status(400).json({error: 'Title must be unique.'})
+      })
+  } else {
+     return res.status(400).json({error: "Title required."})
+  }
+})
+
+module.exports = router;
