65e1f8990e75ad1fd3eeeb3063df9a3ab424e37e
Graham Thompson <grwthomps@gmail.com>
2020-02-06 16:01:30 -0700

    Create test for creating a new playlist via post request
    Co-Authored-By: Mary Lang <mcat56@users.noreply.github.com>

---
+++ tests/playlist.spec.js
@@ -0,0 +1,34 @@
+var shell = require('shelljs');
+var request = require("supertest");
+var app = require('../app');
+
+const environment = process.env.NODE_ENV || 'test';
+const configuration = require('../knexfile')[environment];
+const database = require('knex')(configuration);
+
+describe('Test playlists endpoint', () => {
+  beforeEach(async () => {
+    await database.raw('truncate table playlists cascade');
+
+    let playlist = {id: 3756, title: 'Fun Time Jamz'}
+
+    await database('playlists').insert(playlist)
+  });
+
+  afterEach(() => {
+    database.raw('truncate table playlists cascade')
+  });
+
+  test('User can post a new playlist', async () => {
+    const res = await request(app)
+      .post('/api/v1/playlists')
+      .send({title: 'Study Jamz'});
+
+      expect(res.body).toHaveProperty('id')
+      expect(res.body).toHaveProperty('title')
+      expect(res.body).toHaveProperty('createdAt')
+      expect(res.body).toHaveProperty('updatedAt')
+      expect(res.status).toBe(201)
+  });
+
+})
