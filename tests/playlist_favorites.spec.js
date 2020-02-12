1d9cb6005a48719a28ff53bcd333d413dffc0b22
Graham Thompson <grwthomps@gmail.com>
2020-02-11 17:14:39 -0700

    Add sad path tests for adding a favorite to a playlist
    Co-Authored-By: Mary Lang <mcat56@users.noreply.github.com>

--- tests/playlist_favorites.spec.js
+++ tests/playlist_favorites.spec.js
@@ -33,4 +33,27 @@ describe('Test post playlist favorites endpoint', () => {
       expect(res.status).toBe(201)
   });
 
+  test('User cannot post a new playlist favorite with an invalid playlist id', async () => {
+    const res = await request(app)
+      .post('/api/v1/playlists/34860127/favorites/6523')
+
+      expect(res.body.error_message).toEqual("Playlist not found")
+      expect(res.status).toBe(404)
+  });
+
+  test('User cannot post a new playlist favorite with an invalid favorite id', async () => {
+    const res = await request(app)
+      .post('/api/v1/playlists/3756/favorites/9093495')
+
+      expect(res.body.error_message).toEqual("Favorite not found")
+      expect(res.status).toBe(404)
+  });
+  
+  test('User cannot post a new playlist favorite with letters for an id', async () => {
+    const res = await request(app)
+      .post('/api/v1/playlists/3756/favorites/regt')
+
+      expect(res.body.error_message).toEqual("Bad request. Ids must be a number.")
+      expect(res.status).toBe(400)
+  });
 })
