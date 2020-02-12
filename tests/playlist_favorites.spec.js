259751ff781dfd324b14af5cee91fc0e01ffc26a
Mary Lang <mcl86@cornell.edu>
2020-02-11 17:48:59 -0700

    Add condition to playlists router to catch error for favorite cannot be added twice

--- tests/playlist_favorites.spec.js
+++ tests/playlist_favorites.spec.js
@@ -13,10 +13,14 @@ describe('Test post playlist favorites endpoint', () => {
     await database.raw('truncate table favorites cascade');
 
     let playlist = {id: 3756, title: 'Fun Time Jamz'}
-    let favorite = {id: 6523, title: 'Hakuna Matata', artistName: 'Timon & Pumba', rating: 100, genre: 'Soundtrack'}
+    let favorite1 = {id: 6523, title: 'Hakuna Matata', artistName: 'Timon & Pumba', rating: 100, genre: 'Soundtrack'}
+    let favorite2 = {id: 4869, title: 'Let it Go', artistName: 'Elsa', rating: 20, genre: 'Soundtrack'}
+    let playlist_favorite = { playlist_id: 3756, favorite_id: 4869}
 
     await database('playlists').insert(playlist)
-    await database('favorites').insert(favorite)
+    await database('favorites').insert(favorite1)
+    await database('favorites').insert(favorite2)
+    await database('playlist_favorites').insert(playlist_favorite)
   });
 
   afterEach(() => {
@@ -48,12 +52,20 @@ describe('Test post playlist favorites endpoint', () => {
       expect(res.body.error_message).toEqual("Favorite not found")
       expect(res.status).toBe(404)
   });
-  
+
   test('User cannot post a new playlist favorite with letters for an id', async () => {
     const res = await request(app)
       .post('/api/v1/playlists/3756/favorites/regt')
 
       expect(res.body.error_message).toEqual("Bad request. Ids must be a number.")
+      expect(res.status).toBe(400)
+  });
+
+  test('User cannot add favorite to playlist more than once', async () => {
+    const res = await request(app)
+      .post('/api/v1/playlists/3756/favorites/4869')
+
+      expect(res.body.error_message).toEqual("Favorite cannot be added to playlist twice")
       expect(res.status).toBe(400)
   });
 })
